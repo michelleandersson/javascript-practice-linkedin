8239d21801bfdb4aa063490f01bccfa080dc7c6a
Michelle Andersson <michelleandersson@live.com>
2021-04-14 14:07:36 +0200

    practice creatin nested objects

--- Practice/03_07/script.js
+++ Practice/03_07/script.js
@@ -7,3 +7,24 @@
  * - Find an object that has another object inside of it to create a nested object.
  * - Test your objects in the browser console by accessing the entire object and its specific properties.
  */
+
+
+const cellPhone = {
+    color: "blue", 
+    sizeCM: 20, 
+    apps: {
+        game: "Gems and Goblins", 
+        social: "Facebook", 
+        work: "Teams"
+    },
+    powerOn: true, 
+    togglePower: function(powerStatus) {
+        this.powerOn = powerStatus; 
+    }
+}
+
+console.log(cellPhone); 
+console.log("my cell phone is", cellPhone.color); 
+console.log("it is", cellPhone.sizeCM, "cm tall"); 
+console.log("it contains the following apps:", cellPhone.apps.game, ",",  cellPhone.apps.social, ", and", cellPhone.apps.work); 
+ 
