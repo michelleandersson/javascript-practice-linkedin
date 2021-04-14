26aeb59c701661ebe53bcf734165b2f770628091
Michelle Andersson <michelleandersson@live.com>
2021-04-14 13:16:05 +0200

    dot vs bracket notation 0306

--- 03_06/script.js
+++ 03_06/script.js
@@ -22,3 +22,9 @@ const backpack = {
 };
 
 console.log("The backpack object:", backpack);
+console.log("Digging deeper with dotnotation, we can get the straplength of L:", backpack.strapLength.left); 
+console.log("Bracketnotation is clunkier but still gives us the value for pockets:", backpack["pocketNum"]);
+
+var query = "pocketNum"; 
+console.log("But bracketnotation allows us to use variables:", backpack[query]); 
+console.log(backpack.pocketNum , "is the same as" , backpack[query]); 
