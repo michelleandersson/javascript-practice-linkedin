b436e7bec75ee4df05802784e4e24e029f96d729
Michelle Andersson <michelleandersson@live.com>
2021-04-15 12:49:12 +0200

    practice simple methods 0309

--- Practice/03_09/script.js
+++ Practice/03_09/script.js
@@ -23,4 +23,26 @@ const backpack = {
     this.strapLength.left = lengthLeft;
     this.strapLength.right = lengthRight;
   },
+  giveNewName: function (newName) {
+    this.name = newName; 
+  }, 
+  giveNewVolume: function (vol) {
+    this.volume = vol; 
+  }, 
+  giveNewColor: function (newColor) {
+    this.color = newColor; 
+  }, 
 };
+
+console.log(backpack.name); 
+backpack.giveNewName("Weekend Backpack");
+console.log(backpack.name);  
+
+
+console.log(backpack.volume); 
+backpack.giveNewVolume(20); 
+console.log(backpack.volume); 
+
+console.log(backpack.color); 
+backpack.giveNewColor("green"); 
+console.log(backpack.color); 
