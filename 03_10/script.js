4a8b12555193546417a2b04a4120cea4411f77cc
Michelle Andersson <michelleandersson@live.com>
2021-04-15 12:58:26 +0200

    practice creating classes 0310

--- 03_10/script.js
+++ 03_10/script.js
@@ -14,5 +14,15 @@ const everydayPack = new Backpack(
   false
 );
 
+const weekendPack = new Backpack( 
+  "Weekend Backpack", 
+  15, 
+  "green",
+  20, 
+  20,
+  true
+)
+
 console.log("The everydayPack object:", everydayPack);
 console.log("The pocketNum value:", everydayPack.pocketNum);
+console.log("The weekendpack object:", weekendPack); 
