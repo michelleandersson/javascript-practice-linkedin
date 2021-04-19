4cc574d2fe6e6ee1526e9f5ed8dfd35cea01eda1
Michelle Andersson <michelleandersson@live.com>
2021-04-19 15:09:20 +0200

    practice finding elements w/ queryselector

--- Practice/05_04/script.js
+++ Practice/05_04/script.js
@@ -2,3 +2,11 @@
  * Note: This file is intentionally empty.
  * You can use it to test your skills at traversing the DOM using JavaScript.
  */
+
+document.querySelector(".backpack__volume").style.backgroundColor ="red";
+document.querySelector(".backpack__color").style.backgroundColor = "orange";
+document.querySelector(".backpack__age").style.backgroundColor ="yellow";
+document.querySelector(".backpack__pockets").style.backgroundColor ="green";
+document.querySelector(".backpack__strap").style.backgroundColor ="blue";
+document.querySelector(".feature:nth-of-type(6)").style.backgroundColor ="purple";
+document.querySelector(".backpack__lid").style.backgroundColor ="pink"; 
