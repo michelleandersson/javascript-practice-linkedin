4857b8eb31d179a95629b086042c9e2e2ee400cf
Michelle Andersson <michelleandersson@live.com>
2021-04-20 12:02:04 +0200

    practice modifying elements 0508

--- Practice/05_08/script.js
+++ Practice/05_08/script.js
@@ -5,12 +5,8 @@
  */
 
 
-// document.querySelector(".site-title").style.backgroundColor ="red"; 
-// document.querySelector("img").setAttribute("alt", "a backpack");
-// console.log(document.querySelector("h1").classList);
-// document.querySelector("h1").toggleClass("backpackName"); 
-// console.log(document.querySelector("h1").classList);
 
+//Attributes
 console.log(document.querySelector("img").hasAttribute("src"));
 console.log(document.querySelector("img").hasAttribute("title"));
 console.log(document.querySelector("img").getAttribute("src"));
@@ -22,3 +18,15 @@ document.querySelector("img").setAttribute("title", "images don't need titles");
 console.log(document.querySelector("img").getAttribute("title"));
 document.querySelector("img").removeAttribute("title"); 
 console.log(document.querySelector("img").getAttribute("title")); 
+
+//classes
+
+console.log(document.querySelector("h1").className); 
+console.log(document.querySelector("h1").classList); 
+document.querySelector("h1").classList.add("newClass"); 
+console.log(document.querySelector("h1").classList); 
+document.querySelector("h1").classList.remove("backpackName"); 
+console.log(document.querySelector("h1").classList); 
+document.querySelector("h1").classList.toggle("backpackName"); 
+console.log(document.querySelector("h1").classList); 
+console.log(document.querySelector("h1").classList); 
