62c16af8d0c8db176edb46cba8b3557c436c2f9a
Michelle Andersson <michelleandersson@live.com>
2021-04-22 08:51:51 +0200

    fix scope problems with let 0604

--- 06_04/script.js
+++ 06_04/script.js
@@ -3,18 +3,22 @@
  * @link https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Statements/let
  */
 
-var color = "purple";
+let color = "purple";
 
 document.querySelector(".left").style.backgroundColor = color;
 document.querySelector(".left .color-value").innerHTML = color;
 
 color = "skyblue";
 
+//let variables are local and this color only exists within this function
 function headingColor() {
-  color = "blue";
-  document.querySelector(".title").style.color = color;
+  let color = "blue";
+  let titleColor = "green"
+  document.querySelector(".title").style.color = titleColor;
+  console.log("let variable inside function: ", titleColor); 
 }
-
+//outside function: error: uncaught reference error, not defined
+// console.log("let variable outside function: ", titleColor); 
 headingColor();
 
 document.querySelector(".right").style.backgroundColor = color;
