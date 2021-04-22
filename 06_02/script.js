2c0127e00908588741a000c4dc489c4afe4950ab
Michelle Andersson <michelleandersson@live.com>
2021-04-22 08:35:37 +0200

    practice basic var 0602

--- 06_02/script.js
+++ 06_02/script.js
@@ -2,3 +2,18 @@
  * The var statement
  * @link https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Statements/var
  */
+
+var container = 5; 
+console.log(container); 
+
+container = "red"; 
+console.log(container); 
+
+var x = 4, y = 5, z = "blue"; 
+
+console.log(x, y, z); 
+
+var empty; 
+console.log(empty); 
+empty = "now it has content"; 
+console.log(empty); 
